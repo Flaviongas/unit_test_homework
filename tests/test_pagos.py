6ed2e97d3c7434732026d7fabe263fab70956f08
Flaviongas <flavyjala@hotmail.com>
2025-04-17 02:53:20 +0000

    Primer commit desde Colab con CI

---
+++ tests/test_pagos.py
@@ -0,0 +1,12 @@
+import random
+from unittest.mock import Mock
+ 
+ 
+days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri' , 'sat']
+ 
+random.choice = Mock(return_value='wed')
+print(random.choice())
+
+#using patch
+with patch('random.choice', return_value='wed'):
+    print(random.choice(days))
